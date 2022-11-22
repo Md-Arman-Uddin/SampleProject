6b3197fedb17855a7533cb93a0e92b8fddb3ad9f
Md-Arman-uddin <uddinmdarman0@gmail.com>
2022-11-22 09:44:19 +0530

    Create DemoApp in java

---
+++ DemoApp/src/DemoApp.java
@@ -0,0 +1,18 @@
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] a = {10, 20, 30, 40};
+		for(int ele: a) {
+			System.out.println(ele);
+		}
+		
+		String name = "Sachin";
+		int count = name.toUpperCase().length();
+		System.out.println("The length of the String is: " + count);
+
+	}
+
+}
